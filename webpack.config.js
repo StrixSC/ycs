1d920460980dfb949d7db0fa79b1f57c00fcf789
Wail Ayad <wailay@hotmail.fr>
2020-10-02 23:15:23 -0400

    added linter

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,26 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: './src/main.ts',
-    background: './src/background.ts',
-  },
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  plugins: [
-  ]
+    entry: {
+        main: './src/main.ts',
+        background: './src/background.ts',
+    },
+    devtool: 'inline-source-map',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'public'),
+    },
+    plugins: [],
 };
